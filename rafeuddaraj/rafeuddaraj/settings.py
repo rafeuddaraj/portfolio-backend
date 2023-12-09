d77084744159aa797b2fc35602efc79c41cb7bac
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-12-09 22:42:59 +0600

    server

--- rafeuddaraj/rafeuddaraj/settings.py
+++ rafeuddaraj/rafeuddaraj/settings.py
@@ -88,6 +88,8 @@ WSGI_APPLICATION = 'rafeuddaraj.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
