e236a36bd4fa41c213be866ea9c73ba0516d1bff
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-12-24 20:11:48 +0600

    solve date time

--- rafeuddaraj/routine/models.py
+++ rafeuddaraj/routine/models.py
@@ -10,8 +10,8 @@ class ExamSchedule(models.Model):
     )
 
     date = models.DateTimeField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
     subject = models.CharField(max_length=255)
     subject_code = models.CharField(max_length=20)
     room_number = models.CharField(max_length=10)
@@ -19,17 +19,3 @@ class ExamSchedule(models.Model):
 
     def __str__(self):
         return f"{self.date} - {self.subject}"
-
-    def save(self, *args, **kwargs):
-        current_datetime = timezone.now()
-        exam_datetime_start = timezone.make_aware(timezone.datetime.combine(self.date, self.start_time))
-        exam_datetime_end = timezone.make_aware(timezone.datetime.combine(self.date, self.end_time))
-
-        if current_datetime >= exam_datetime_start and current_datetime < exam_datetime_end:
-            self.status = 2  # InProgress
-        elif current_datetime >= exam_datetime_end:
-            self.status = 3  # Complete
-        else:
-            self.status = 1  # Pending
-
-        super().save(*args, **kwargs)
